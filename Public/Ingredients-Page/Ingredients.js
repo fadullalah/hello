9dd31c5c8236646c77c7d3c5b086726aed17c086
fadullalah <54377293+fadullalah@users.noreply.github.com>
2023-04-11 12:45:44 +0300

    Add files via upload

---
+++ Public/Ingredients-Page/Ingredients.js
@@ -0,0 +1,18 @@
+var addBtn = document.querySelector(".plus-button");
+var modal = document.querySelector(".modal");
+var closeModalBtn = document.querySelector(".close-button");
+var profitContainer = document.querySelector(".profit-container-c");
+var componentPriceContainer = document.querySelector(".price-container-c");
+
+addBtn.addEventListener("click", function() {
+  modal.style.display = "block";
+});
+
+closeModalBtn.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
+
+
+
+
